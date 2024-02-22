5e00f2198e5958df783057e94b562136093f3ff4
Evgeniya <yoojinbaek25@gmail.com>
2024-02-22 14:49:55 +0300

    commit2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import lombok.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Human human = new Human("John", "Smith");
+        System.out.println(human);
+    }
+}
